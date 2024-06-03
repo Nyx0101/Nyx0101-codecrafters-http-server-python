3e654a75d2b4ca0ab782bc3add2b8acf5f04e06b
root <root@KaliBridge.com>
2024-06-03 08:49:48 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -18,7 +18,9 @@ def main():
                 response = b"HTTP/1.1 200 OK\r\n\r\n"
             if "echo" in path[1]:
                 string = path[1].strip("/echo/")
-                response = f"HTTP/1.1 200 OK/r/n/Content-Type: text/plain/r/nContent-Length: {len(string)}\r\n\r\n{string}".encode()
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}".encode()
+                print(f"First par {path}")
+                
             print(f"Recieved: {val}")
             conn.sendall(response)
     
