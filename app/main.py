703121635b0dd51405273d66e1716726a9cb4f5f
root <root@KaliBridge.com>
2024-06-03 08:45:38 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -6,20 +6,22 @@ def main():
     
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-        
     conn, addr = server_socket.accept()
-    print(f"Connection from {addr[0]} port {addr[1]}")
-        
-    data = conn.recv(1024).decode("utf-8")
-    print(data)
-        
-    path = data.split(" ")[1]
-        
-    if path == "/":
-        conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
-    else:
-        conn.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
-        print(f"Sent response: {response}")
+    with conn:
+        val = conn.recv(1024)
+        pars = val.decode()
+        args = pars.split("\r\n")
+        response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+        if len(args) > 1:
+            path = args[0].split(" ")
+            if path == "/":
+                response = b"HTTP/1.1 200 OK\r\n\r\n"
+            if "echo" in path[1]:
+                string = path[1].strip("/echo/")
+                response = f"HTTP/1.1 200 OK/r/n/Content-Type: text/plain/r/nContent-Length: {len(string)}\r\n\r\n{string}".encode()
+            print(f"Recieved: {val}")
+            conn.sendall(response)
+    
      
 if __name__ == "__main__":
     main()
