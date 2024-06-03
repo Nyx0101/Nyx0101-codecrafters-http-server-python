fd8ad26b7f6d339b23c860082cbc4d4842c2fbf6
root <root@KaliBridge.com>
2024-06-03 07:52:02 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,50 +1,25 @@
 import socket
 
+
 def main():
     print("Logs from your program will appear here!")
     
-    try:
-        # Create the server socket
-        server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-        print("Server started on localhost:4221")
+    
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
         
-        while True:
-            try:      
-                # Wait for client a client connection
-                conn, addr = server_socket.accept()
-                print(f"Connection from {addr[0]} port {addr[1]}")
+    conn, addr = server_socket.accept()
+    print(f"Connection from {addr[0]} port {addr[1]}")
         
-                # Receive data
-                data = comm.recv(1024).decode("utf-8")
-                print(f"Received data: {data}")
+    data = comm.recv(1024).decode("utf-8")
+    print(data)
         
-                # Extract the path from the HTTP request
-                path = data.split(" ")[1]
+    path = data.split(" ")[1]
         
-                # Determine response based on the path
-                if path == "/":
-                    response = b"HTTP/1.1 200 OK\r\n\r\n"
-                else:
-                    response = b"HTTP/1.1 404 Not Found\rn\r\n"
-            
-                # Send the response
-                comm.sendall(response)
-                print(f"Sent response: {response}")
-             
-        except Exception as e:
-            print(f"An error occured while handling the connection: {e}")
-        finally:
-            # Ensure the client socket is closed
-            comm.close()
-            print("Client connection closed")
-        
-     except Exception as e:
-        print(f"An error occurred: {e}")
-         
-    finally:
-        # Ensure the server socket is closed
-        server_socket.close()
-        print("Server socket closed")
+    if path == "/":
+        conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        comm.sendall(response)
+        print(f"Sent response: {response}")
      
 if __name__ == "__main__":
     main()
