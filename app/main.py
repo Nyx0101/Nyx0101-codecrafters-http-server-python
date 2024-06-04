73f349b33687395f1072db7a5fc9512fd2a90a75
root <root@KaliBridge.com>
2024-06-04 14:36:32 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ def main():
         
         if path =="/":
             response = "HTTP/1.1 200 OK\r\n\r\n".encode()
-        elif path.startswith("\echo"):
+        elif path.startswith("/echo"):
             echo_message = path[6:]
             response = (f"HTTP/1.1 200 OK\r\n"
                         f"Content-Type: text/plain\r\n"
@@ -25,29 +25,29 @@ def main():
                         f"{echo-message}").encode()
         elif path.startswith("/user-agent"):
             user_agent = ""
-                for header in req:
-                    if header.startswith("User-Agent"):
-                        user_agent = header.split(": ")[1]
-                        break
-                if user_agent:
-                    response = (f"HTTP/1.1 200 OK\r\n"
-                                f"Content-Type: text/plain\r\n"
-                                f"Content-Length: {len(user_agent)}\r\n\r\n"
-                                f"{user_agent}").encode()
-                else:
-                    response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
+            for header in req:
+                if header.startswith("User-Agent"):
+                    user_agent = header.split(": ")[1]
+                    break
+            if user_agent:
+                response = (f"HTTP/1.1 200 OK\r\n"
+                            f"Content-Type: text/plain\r\n"
+                            f"Content-Length: {len(user_agent)}\r\n\r\n"
+                            f"{user_agent}").encode()
             else:
                 response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
-                
-            print(f"Sending response: {response.decode()}")
-            client.send(response)
         else:
-            print("No request data received.")
-            response = "HTTP/1.1 400 Bad Request\r\n\r\n".encode()
-            client.send(response)
+            response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
+                
+        print(f"Sending response: {response.decode()}")
+        client.send(response)
+    else:
+        print("No request data received.")
+        response = "HTTP/1.1 400 Bad Request\r\n\r\n".encode()
+        client.send(response)
             
-        client.close()
-        print("Connection closed")
+    client.close()
+    print("Connection closed")
         
 if__name__ == "__main__":
     main()
