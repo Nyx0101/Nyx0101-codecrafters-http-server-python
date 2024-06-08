a26ee6012680112af48bbaeba212a706fb5d979b
root <root@KaliBridge.com>
2024-06-07 22:51:00 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import socket
 import threading
-from typing import Dict
 
+from typing import Dict
 
 class HTTPRequest:
     method: str
@@ -13,68 +13,66 @@ class HTTPRequest:
         self.headers = {}
         
     @staticmethod
-    def from_bytes(request_bytes: bytes) -> "HTTPRequest":
+    def from_bytes(reques_bytes: bytes) -> "HTTPRequest":
         request = HTTPRequest()
         
         line_iter = iter(request_bytes.split(b"\r\n"))
+        
         line = next(line_iter)
-        request.method, request.path, request.http_version = [
-            b.decode() for b in line.split(b" ")
-        ]
-
+        
+        request.method, request.path, request.http_version = [b.decode() for b in line.split(b" ")]
+        
         for line in line_iter:
             if len(line.strip()) == 0:
                 continue
-            
+                
             key, value = line.split(b":", maxsplit=1)
-            request.headers[key.decode()] = value.decode().strip
+            
+            request.headers[key.decode()] = value.decode().strip()
             
         return request
         
 def main() -> None:
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    server_socket = socket.creater_server(("localhost", 4221), reuse_port=True)
+    
     while True:
         sock, response_addr = server_socket.accept()
-        request_handler(sock)
-        t = threading.Thread(target=lambda: request_handler(sock))
+        
+        t = threading.Thread(target=request_handler, args=(sock,))
+        
         t.start()
         
 def request_handler(sock: socket.socket) -> None:
     request_bytes = sock.recv(1024)
-    request = HTTPRequest.from_bytes(request_bytes)
+    
+    request = HTTPRequest.from_bytes(request_bytes) 
     
     headers = {}
     response_body = ""
+    response_code ="404 Not Found"
     
-    response_code = "404 Not Found"
     if request.path == "/":
         response_code = "200 OK"
     elif request.path.startswith("/echo/"):
         response_code = "200 OK"
-        response_body = request.path[len("/echo/") :]
-        
-        
+        response_body = request.path[len("/echo/"):]
         headers["Content-Type"] = "text/plain"
         headers["Content-Length"] = len(response_body)
     elif request.path.startswith("/user-agent"):
         response_code = "200 OK"
         response_body = request.headers.get("User-Agent")
+        headers["Content-Type"] = "text/plain"
+        headers["Content-Length"] = len(response_body)
         
-    headers["Content-Type"] = "text/plain"
-    headers["Content-Length"] = len(response_body)
-        
-    response_contents = [
-        f"{request.http_version} {response_code}",
-        *[f"{key}: {value}" for key, value in headers.items()],
-        "",
-        response_body,
-    ]
+    response_contents = [f"{request.http_version} {response_code}", *[f"{key}: {value}" for key, value in headers.items()], "", response_body]
     
-    sock.send("\r\n".join(response_contents).encode())
+    sock.send("\r\n".join(respose_contents).encode())
+    
     sock.close()
     
 if __name__ == "__main__":
     main()
+
                 
                 
            
