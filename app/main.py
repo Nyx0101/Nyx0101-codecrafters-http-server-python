dc44005de7b18a20c2061c067c550633eeda3665
root <root@KaliBridge.com>
2024-06-08 18:36:56 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,80 +1,41 @@
 import socket
 import threading
 
-from typing import Dict
-
-class HTTPRequest:
-    method: str
-    path: str
-    http_version: str
-    headers: Dict[str, str]
-    
-    def __init__(self) -> None:
-        self.headers = {}
-        
-    @staticmethod
-    def from_bytes(reques_bytes: bytes) -> "HTTPRequest":
-        request = HTTPRequest()
-        
-        line_iter = iter(request_bytes.split(b"\r\n"))
-        
-        line = next(line_iter)
-        
-        request.method, request.path, request.http_version = [b.decode() for b in line.split(b" ")]
-        
-        for line in line_iter:
-            if len(line.strip()) == 0:
-                continue
-                
-            key, value = line.split(b":", maxsplit=1)
-            
-            request.headers[key.decode()] = value.decode().strip()
-            
-        return request
-        
-def main() -> None:
+def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    conn, address = server_socket.accept()
+def handle_request(conn):
+    with conn:
+        data = conn.recv(4096)
+        parsed_request = str(data.decode()).split()
+        method, path, *_ = parsed_request
+        
+        if path =="/":
+            conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        elif path.startswith("/echo") or path.startswith("/user-agent"):
+        if path.startswith("/echo"):
+            body = path.split("/")[-1]
+        else:
+            body = parsed_request[parsed_request.index("User-Agent:") + 1]
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(body)}\r\n\r\n{body}"
+        conn.sendall(response.encode())
+    else:
+        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        
+        
+def main():
+    server_socket = socket.create_server(("localhost, 4221), reuse_port=True) 
     
     while True:
-        sock, response_addr = server_socket.accept()
-        t = threading.Thread(target=request_handler, args=(sock,))
-        t.start()
+        conn, address = server_socket.accept()
+        thread = threading.Thread(target=lamda: handle_request(conn))
+        thread.start()
         
-def request_handler(sock: socket.socket) -> None:
-    request_bytes = bytearray()
-    while True:
-        chunk = sock.recv(1024)
-        if not chunk:
-            break
-        request_bytes.extend(chunk)
-    
-    request = HTTPRequest.from_bytes(request_bytes) 
-    
-    headers = {}
-    response_body = ""
-    response_code ="404 Not Found"
-    
-    if request.path == "/":
-        response_code = "200 OK"
-    elif request.path.startswith("/echo/"):
-        response_code = "200 OK"
-        response_body = request.path[len("/echo/"):]
-        headers["Content-Type"] = "text/plain"
-        headers["Content-Length"] = len(response_body)
-    elif request.path.startswith("/user-agent"):
-        response_code = "200 OK"
-        response_body = request.headers.get("User-Agent")
-        headers["Content-Type"] = "text/plain"
-        headers["Content-Length"] = len(response_body)
         
-    response_contents = [f"{request.http_version} {response_code}", *[f"{key}: {value}" for key, value in headers.items()], "", response_body]
-    
-    sock.send("\r\n".join(respose_contents).encode())
-    
-    sock.close()
-    
 if __name__ == "__main__":
     main()
+    
+    
 
                 
                 
