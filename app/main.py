59ee613e30510c72a630dff0a606d4bc59f18ad8
root <root@KaliBridge.com>
2024-06-10 14:15:28 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ def handle_request(conn):
         
         
 def main():
-    server_socket = socket.create_server(("localhost, 4221), reuse_port=True) 
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True) 
     
     while True:
         conn, address = server_socket.accept()
