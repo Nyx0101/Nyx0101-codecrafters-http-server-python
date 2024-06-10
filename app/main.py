2fac9596fe9f21f7c1614b5d0c36c2e23a323d03
root <root@KaliBridge.com>
2024-06-10 19:44:16 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,38 +1,81 @@
+# Uncomment this to pass the first stage
+
 import socket
 import threading
-import logging
+from typing import Dict
 
-logging .basicConfig(level=logging.INFO)
+class HTTPRequest:
+    method: str
+    path: str
+    http_version: str
+    headers: Dict[str, str]
 
-def handle_client(client_socket):
-    try:
-        request = client_socket.recv(4096)
-        if not request:
-            return        
-        request = request.decode("utf-8")
-        logging.info(f"Received request: {request}")      
-        response = "HTTP/1.1 200 OK\r\n"
-        response += "Content-Type: text/plain\r\n\r\n"
-        response_length = len(response.encode("utf-8"))
-        response += "Content-Length: {}\r\n\r\n".format(response_length)
-        response += "Hello, World!\r\n"
-        client_socket.sendall(response.encode("utf-8"))
-    except Exception as e:
-        logging.error(f"Error handling client: {e}")
-    finally:
-        client_socket.close()
-    
-def main():
+    def __init__(self) -> None:
+        self.headers = {}
+
+    @staticmethod
+    def from_bytes(request_bytes: bytes) -> "HTTPRequest":
+        request = HTTPRequest()
+        line_iter = iter(request_bytes.split(b"\r\n"))
+        line = next(line_iter)
+        request.method, request.path, request.http_version = [
+            b.decode() for b in line.split(b" ")
+        ]
+
+        for line in line_iter:
+            if len(line.strip()) == 0:
+                continue
+
+            key, value = line.split(b":", maxsplit=1)
+            request.headers[key.decode()] = value.decode().strip()
+
+        return request
+
+def main() -> None:
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    logging.info("Server started")
-    
+
     while True:
-        conn, address = server_socket.accept()
-        logging.info(f"Connected by {address}")
-        client_thread = threading.Thread(target=handle_client, args=(conn,))
-        client_thread.start()
+        sock, response_addr = server_socket.accept()  # wait for client
+        request_handler(sock)
+        t = threading.Thread(target=lambda: request_handler(sock))
+        t.start()
+
+def request_handler(sock: socket.socket) -> None:
+
+    request_bytes = sock.recv(1024)
+    request = HTTPRequest.from_bytes(request_bytes)
+    
+    headers = {}
+    response_body = ""
+    
+    response_code = "404 Not Found"
+    if request.path == "/":
+        response_code = "200 OK"
+    elif request.path.startswith("/echo/"):
+        response_code = "200 OK"
+        response_body = request.path[len("/echo/") :]
         
+        headers["Content-Type"] = "text/plain"
+        headers["Content-Length"] = len(response_body)
+    elif request.path.startswith("/user-agent"):
+        response_code = "200 OK"
+        response_body = request.headers.get("User-Agent")
+
+        headers["Content-Type"] = "text/plain"
+        headers["Content-Length"] = len(response_body)
+
+    response_contents = [
+        f"{request.http_version} {response_code}",
+        *[f"{key}: {value}" for key, value in headers.items()],
+        "",
+        response_body,
+    ]
+
+    sock.send("\r\n".join(response_contents).encode())
+    sock.close()
+
 if __name__ == "__main__":
+
     main()
     
     
