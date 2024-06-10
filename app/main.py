3638accfcf2939aa131469efab7991a856a884fc
root <root@KaliBridge.com>
2024-06-10 16:12:53 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -9,7 +9,10 @@ def handle_client(client_socket):
     request = request.decode("utf-8")
     print(f"Received request: {request}")
          
-    response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!\r\n"
+    response = "HTTP/1.1 200 OK\r\n"
+    response += "Content-Type: text/plain\r\n\"
+    response += "Content-Length: 13\r\n\r\n"
+    response += "Hello, World!\r\n"
     client_socket.sendall(response.encode("utf-8"))
      
     client_socket.close()
