cc7394f449d69804f074a5cc4bc91fa3f6968646
root <root@KaliBridge.com>
2024-06-18 13:22:29 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -24,7 +24,7 @@ def main():
                     body = f.read()
                 response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(body)}\r\n\r\n{body}".encode()        
             except Exception as e:
-                response = f"HTTP/1.1 404 Not Found\r\n\r\n".encode
+                response = f"HTTP/1.1 404 Not Found\r\n\r\n".encode()
         else:
             response = "HTTP?1.1 404 Not Found\r\n\r\n".encode()
         client.send(response)
