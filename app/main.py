6d5902aec7f8d2c6affbd3c97a84a843eb2e943d
root <root@KaliBridge.com>
2024-06-18 13:19:23 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -5,17 +5,16 @@ import sys
 
 def main():
     def handle_req(client, addr):
-        data = client.recv(1024)
+        data = client.recv(1024).decode()
         req = data.split("\r\n")
         path = req[0].split(" ")[1]
         if path == "/":
             response = "HTTP/1.1 200 OK\r\n\r\n".encode()
         elif path.startswith("/echo"):
-            user_agent = req[2].split(": ")[1]
-            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[path[6]}".encode()
+            response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}".encode()
         elif path.startswith("/user-agent"):
-            user_agent = req[2].split(": "[1]
-            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length = {len(user-agent)}\r\n\r\n{user_agent}".encode()
+            user_agent = req[2].split(": ")[1]
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode()
         elif path.startswith("/files"):
             directory = sys.argv[2]
             filename = path[7:]
@@ -25,7 +24,9 @@ def main():
                     body = f.read()
                 response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(body)}\r\n\r\n{body}".encode()        
             except Exception as e:
-            response = f"HTTP/1.1 404 Not Found\r\n\r\n".encode
+                response = f"HTTP/1.1 404 Not Found\r\n\r\n".encode
+        else:
+            response = "HTTP?1.1 404 Not Found\r\n\r\n".encode()
         client.send(response)
         
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
@@ -33,7 +34,9 @@ def main():
         client, addr = server_socket.accept()
         threading.Thread(target=handle_req, args=(client, addr)).start()
         
-if __name__ =="__main__":
+        
+        
+if __name__ == "__main__":
     main()  
 
     
