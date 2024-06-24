5b864b8ac138929b26cba77df5699850db5b1b9b
root <root@KaliBridge.com>
2024-06-24 15:34:28 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -2,13 +2,16 @@ import asyncio
 import argparse
 import re
 import sys
-from aiohttp import web
+from asyncio.streams import StreamReader, StreamWriter
 from pathlib import Path
+
 
 GLOBALS = {}
 
+
 def stderr(*args, **kwargs):
     print(*args, **kwargs, file=sys.stderr)
+
 
 def parse_request(content: bytes) -> tuple[str, str, dict[str, str], str]:
     first_line, *tail = content.split(b"\r\n")
@@ -18,6 +21,7 @@ def parse_request(content: bytes) -> tuple[str, str, dict[str, str], str]:
         key, value = line.split(b": ")
         headers[key.decode()] = value.decode()
     return method.decode(), path.decode(), headers, b"".join(tail).decode()
+
 
 def make_response(
     status: int,
@@ -30,39 +34,58 @@ def make_response(
         201: "CREATED",
         404: "NOT FOUND",
     }
+
     return b"\r\n".join(
         map(
             lambda i: i.encode(),
             [
-                f"HTTP/1.1 {status} {msg[status]}".encode(),
-                *[f"{k}: {v}".encode() for k, v in headers.items()],
-                f"Content-Length: {len(body)}".encode(),
-                "".encode(),
-                body.encode(),
+                f"HTTP/1.1 {status} {msg[status]}",
+                *[f"{k}: {v}" for k, v in headers.items()],
+                f"Content-Length: {len(body)}",
+                "",
+                body,
             ],
         ),
     )
 
-async def handle(request):
-    method, path, headers, body = parse_request(await request.read())
-    
+
+async def handle_connection(reader: StreamReader, writer: StreamWriter) -> None:
+    _, path, headers, _ = parse_request(await reader.read(2**16))
+    method, path, headers, body = parse_request(await reader.read(2**16))
+
     if re.fullmatch(r"/", path):
-        return web.Response(text="", status=200)
+        writer.write(b"HTTP/1.1 200 OK\r\n\r\n")
+        stderr(f"[OUT] /")
     elif re.fullmatch(r"/user-agent", path):
-        ua =  headers.get("User-Agent", "")
-        return web.Response(text=ua, content_type="text/plain", status=200)
+        ua = headers["User-Agent"]
+        writer.write(make_response(200, {"Content-Type": "text/plain"}, ua))
+        stderr(f"[OUT] user-agent {ua}")
     elif match := re.fullmatch(r"/echo/(.+)", path):
         msg = match.group(1)
-        return web.Response(text=msg, content_type="text/plain", status=200)
+        writer.write(make_response(200, {"Content-Type": "text/plain"}, msg))
+        stderr(f"[OUT] echo {msg}")
     elif match := re.fullmatch(r"/files/(.+)", path):
         p = Path(GLOBALS["DIR"]) / match.group(1)
+        if p.is_file():
         if method.upper() == "GET" and p.is_file():
-            return web.Response(body=p.read_text(), content_type="application/octet-stream", status=200)
+            writer.write(
+                make_response(
+                    200,
+                    {"Content-Type": "application/octet-stream"},
+                    p.read_text(),
+                )
+            )
         elif method.upper() == "POST":
             p.write_bytes(body.encode())
-            return web.Response(status=201)
+            writer.write(make_response(201))
+        else:
+            writer.write(make_response(404))
+        stderr(f"[OUT] file {path}")
     else:
-        return web.Response(text="", status=404)
+        writer.write(make_response(404, {}, ""))
+        stderr(f"[OUT] 404")
+    writer.close()
+
 
 async def main():
     parser = argparse.ArgumentParser()
@@ -70,16 +93,16 @@ async def main():
     args = parser.parse_args()
     
     GLOBALS["DIR"] = args.directory
-    
-    app = web.Application()
-    app.add_routes([web.post(r"/{path:.+}", handle), web.get(r"/{path:.+}", handle)])
-    
-    runner = web.AppRunner(app)
-    site = web.TCPSite(runner, "localhost", 4221)
-    
-    await site.start()
-    
+
+    server = await asyncio.start_server(handle_connection, "localhost", 4221)
+    async with server:
+        stderr("Starting server...")
+        stderr(f"--directory {GLOBALS['DIR']}")
+        await server.serve_forever()
+
+
 if __name__ == "__main__":
+
     asyncio.run(main())
 
 
