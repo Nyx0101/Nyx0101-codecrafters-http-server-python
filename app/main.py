20d7a737ec35c136324622d2f03242313b8be3db
root <root@KaliBridge.com>
2024-06-30 21:20:45 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,68 +1,73 @@
-from sys import argv
+ import gzip
+import pathlib
 import socket
+import sys
 import threading
+import types
+from collections.abc import Generator
 
-def handle_request(conn, addr):
-    data = conn.recv(1024).decode("utf-8")
-    request = data.split("\r\n")
-    method = request[0].split(" ")[0]
-    path = request[0].split(" ")[1]
-    body = request[-1]
-    user_agent = ""
-    accept_encoding = ""
+from app.entities import Request, Response, ResponseGenerator
+from app.utils import read_request
+
+
+def process(sock: socket.socket) -> None:
+    request = read_request(sock)
     
-    for line in request:
-        if line.startswith("User-Agent:"):
-            user_agent = line[len("User-Agent: "):]
-        if line.startswith("Accept-Encoding:"):
-            accept_encoding = line[len("Accept-Encoding: "):]
-    
-    encoding = ""
-    if "gzip" in accept_encoding:
-        encoding = "Content-Encoding: gzip\r\n"
-    
-    if path == "/":
-        conn.send(b"HTTP/1.1 200 OK\r\n\r\n")
-    elif path == "/user-agent":
-        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}\r\n"
-        conn.send(response.encode())
-    elif "/files" in path:
-        if method == "POST":
-            directory = argv[2]
-            filename = path[7:]
-            try:
-                with open(f"/{directory}/{filename}", "w") as f:
-                    f.write(f"{body}")
-                response = f"HTTP/1.1 201 Created\r\n\r\n"
-            except Exception as e:
-                response = f"HTTP/1.1 404 Not Found\r\n\r\n"
-            conn.send(response.encode())
-        elif method == "GET":
-            f_name = path.split("/")[-1]
-            try:
-                with open(argv[2] + f_name) as f:
-                    content = f.read()
-                    cont_length = len(content)
-                    response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {str(cont_length)}\r\n\r\n{content}"
-            except FileNotFoundError:
-                response = "HTTP/1.1 404 Not Found\r\n\r\n"
-            conn.send(response.encode())
-    elif path.startswith("/echo"):
-        random_path = path[6:]
-        response = f"HTTP/1.1 200 OK\r\n{encoding}Content-Type: text/plain\r\nContent-Length: {len(random_path)}\r\n\r\n{random_path}\r\n"
-        conn.send(response.encode())
+    if request.target == "/":
+        response = Response()  # HTTP/1.1 200 OK\r\n\r\n
+        
+    elif request.target.startswith("/echo/"):
+        body = request.target[6:].encode()
+        response = Response(body=body)
+
+        accept_encodings = (
+            request.headers["Accept-Encoding"].split(", ")
+            if request.headers.get("Accept-Encoding")
+            else []
+        )
+        if accept_encodings and "gzip" in accept_encodings:
+            response.headers["Content-Encoding"] = "gzip"
+            response.body = gzip.compress(body)
+
+    elif request.target.startswith("/user-agent"):
+        body = (request.headers.get("User-Agent") or "").encode()
+        headers = {"Content-Type": "text/plain", "Content-Length": len(body)}
+        response = Response(headers=headers, body=body)
+
+    elif request.target.startswith("/files/") and request.method.lower() == "get":
+        filepath = pathlib.Path(sys.argv[2]) / request.target[7:]
+        response = Response.from_file(filepath)
+
+    elif request.target.startswith("/files/") and request.method.lower() == "post":
+        filepath = pathlib.Path(sys.argv[2]) / request.target[7:]
+        
+        with open(filepath, "wb") as file:
+            file.write(request.body)
+
+        response = Response(status_code=201, status_text="Created")
+
     else:
-        conn.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
-    
-    conn.close()
+        response = Response(
+            status_code=404, status_text="Not Found"
+        )  #  HTTP/1.1 404 Not Found\r\n\r\n
+
+    if isinstance(response, Response):
+        sock.send(response.to_raw())
+    elif isinstance(response, types.GeneratorType):
+        for data in response:
+            sock.send(data)
+
+    sock.close()
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+
     while True:
-        conn, addr = server_socket.accept()
-        threading.Thread(target=handle_request, args=(conn, addr)).start()
+        sock, addr_info = server_socket.accept()
+        threading.Thread(target=process, args=(sock,)).start()
 
 if __name__ == "__main__":
+
     main()
 
             
